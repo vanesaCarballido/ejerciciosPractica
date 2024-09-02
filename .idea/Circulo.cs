631662aaf4af4c75aa13e7b598013dd37c8a2dd1
isaparedes <isabela.paredes@correo.ucu.edu.uy>
2024-09-02 11:56:16 -0300

    agrego program

--- .idea/Circulo.cs
+++ .idea/Circulo.cs
@@ -1,7 +1,30 @@
+using System;
 namespace DefaultNamespace;
 
 public class Circulo
 {
     private double radio;
-    
+
+    public double Radio
+    {
+        get { return radio; }
+        set { radio = value; }
+    }
+    public Circulo(double radio)
+    {
+        this.radio = radio;
+    }
+
+    public double GetPerimeter()
+    {
+        double perimeter = (radio * 2) * (3.1416);
+        return perimeter;
+    }
+
+    public double GetArea()
+    {
+        double area = (radio * radio) * (3.1416);
+        return area;
+    }
 }
+
